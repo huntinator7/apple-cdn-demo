b799cba409bc725cb326e9f6944b33b269ec7eb5
Hunter Arnell <hunterarnell@Hunters-computer.local>
2020-07-10 04:24:08 -0600

    Added pkg

---
+++ apple-cdn-demo-wasm/pkg/apple_cdn_demo_wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} edges
+* @param {any} closest_edge
+* @param {number} cache1
+* @param {number} cache2
+* @returns {any}
+*/
+export function get_closest_node(edges: any, closest_edge: any, cache1: number, cache2: number): any;
---
+++ apple-cdn-demo-wasm/pkg/apple_cdn_demo_wasm.js
@@ -0,0 +1,2 @@
+import * as wasm from "./apple_cdn_demo_wasm_bg.wasm";
+export * from "./apple_cdn_demo_wasm_bg.js";
